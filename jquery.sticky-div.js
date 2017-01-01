9dc5e3ac6ec98df87ec3f388733798e278804aa8
ADTC <send2adtc@gmail.com>
2017-01-01 23:31:56 +0800

    Add new feature to stick the navbar at the bottom of viewport

--- jquery.sticky-div.js
+++ jquery.sticky-div.js
@@ -5,13 +5,18 @@
       min_screen_width: 0,
       min_screen_height: 0,
       bottom: 0,
+      stick_bottom: false,
       outer_div: null,
       style_css: null
     };
     var options = $.extend(defaults, options);
 
-    // !important to override any current positioning or top.
-    $("<style type='text/css'> .sticky-div{ position:fixed !important; top:" + options.top + "px !important;} </style>").appendTo("head");
+    // !important to override any current positioning or top/bottom.
+    $("<style type='text/css'>"
+      + " .sticky-div-top    { position: fixed !important; top: "    + options.top    + "px !important; }"
+      + " .sticky-div-bottom { position: fixed !important; bottom: " + options.bottom + "px !important; }"
+      + " </style>")
+      .appendTo("head");
 
     var selector = this;
     
@@ -46,19 +51,32 @@
       var div_height = $(this).outerHeight();
       
       var bool_sticky_div = (!options.outer_div || (div_height < $(options.outer_div).height()));
-      bool_sticky_div = bool_sticky_div && (window_top > (div_top - options.top));
-      bool_sticky_div = bool_sticky_div && (div_height < (window_height - options.bottom));
       bool_sticky_div = bool_sticky_div && (window_width >= options.min_screen_width);
       bool_sticky_div = bool_sticky_div && (window_height >= options.min_screen_height);
+      if (options.stick_bottom) {
+        bool_sticky_div = bool_sticky_div && ((window_top + window_height) < ((div_top + div_height) + options.bottom));
+        bool_sticky_div = bool_sticky_div && (div_height < (window_height - options.top));
+      } else {
+        bool_sticky_div = bool_sticky_div && (window_top > (div_top - options.top));
+        bool_sticky_div = bool_sticky_div && (div_height < (window_height - options.bottom));
+      }
       
       if (bool_sticky_div) {
-        $(this).addClass('sticky-div');
+        if (options.stick_bottom) {
+          $(this).addClass('sticky-div-bottom');
+        } else {
+          $(this).addClass('sticky-div-top');
+        }
         if (options.style_css != null) {
           $(this).addClass(options.style_css);
         }
         anchor.height(div_height);
       } else {
-        $(this).removeClass('sticky-div');
+        if (options.stick_bottom) {
+          $(this).removeClass('sticky-div-bottom');
+        } else {
+          $(this).removeClass('sticky-div-top');
+        }
         if (options.style_css != null) {
           $(this).removeClass(options.style_css);
         }
