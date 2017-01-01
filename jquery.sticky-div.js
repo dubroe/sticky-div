b69c33fb5b1fb19869b30a12b3f23f6a0cc50e25
ADTC <send2adtc@gmail.com>
2017-01-01 23:27:21 +0800

    Fix the detection of existing sticky-div class

--- jquery.sticky-div.js
+++ jquery.sticky-div.js
@@ -17,7 +17,7 @@
     
     // add a class to the anchor, and check for it, so that the anchor <div/> is only added the once.
     selector.each(function () {
-      if (!$(this).prev().hasClass(".sticky-anchor")) {
+      if (!$(this).prev().hasClass("sticky-anchor")) {
         $(this).before("<div class='sticky-anchor'></div>");
       }
     });
